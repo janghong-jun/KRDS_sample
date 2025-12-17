4cff04bd475ad0704c8334d137acac23efe43eab
HJ <hj.jang@netive.co.kr>
2025-12-17 11:29:53 +0900

    경로수정

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const postcss = require('gulp-postcss');
 const sassCompiler = require('sass');
 const gulpSass = require('gulp-sass')(sassCompiler);
 const sourcemap = require('gulp-sourcemaps');
@@ -10,31 +11,45 @@ const ssi = require('gulp-ssi');
 const autoprefix = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
 
+const removeEmptyRulesWithComments = () => {
+  return {
+    postcssPlugin: 'remove-empty-rules-with-comments',
+    Once(root) {
+      root.walkRules((rule) => {
+        if (rule.nodes && rule.nodes.every((node) => node.type === 'comment')) {
+          rule.remove();
+        }
+      });
+    },
+  };
+};
+removeEmptyRulesWithComments.postcss = true;
+
 const src = './src';
 const dist = './dist';
 
 const path = {
   main: src,
   scssEntry: [
-    `${src}/resource/scss/output.scss`,
-    `${src}/resource/scss/plugin/swiper-bundle.min.scss`,
+    `${src}/resources/scss/output.scss`,
+    `${src}/resources/scss/plugin/swiper-bundle.min.scss`,
+    `${src}/guide/resources/css/guide.scss`,
   ],
-  scssWatch: `${src}/resource/scss/**/*.scss`,
-  css: `${src}/resource/css`,
-  js: `${src}/resource/js/**/*.js`,
-  images: `${src}/resource/img/**/*.{jpg,jpeg,png,gif,svg,webp,ico}`,
-  font: `${src}/resource/fonts/**/*.{otf,woff,woff2}`,
+  scssWatch: `${src}/resources/scss/**/*.scss`,
+  css: `${src}/resources/css`,
+  js: `${src}/resources/js/**/*.js`,
+  images: `${src}/resources/img/**/*.{jpg,jpeg,png,gif,svg,webp,ico}`,
+  font: `${src}/resources/fonts/**/*.{otf,woff,woff2}`,
   html: [`${src}/**/*.html`, `!${src}/components/**/*.html`],
 };
 
 const clean = () => del([dist]);
-
 /* =========================
    DEV SCSS (최속)
 ========================= */
 gulp.task('sass', () => {
   return gulp
-    .src(path.scssEntry, { base: `${src}/resource/scss` })
+    .src(path.scssEntry, { base: `${src}/resources/scss` })
     .pipe(gulpSass({ outputStyle: 'expanded' }).on('error', gulpSass.logError))
     .pipe(gulp.dest(path.css))
     .pipe(browser.stream({ match: '**/*.css' }));
@@ -58,7 +73,7 @@ gulp.task('serv', () => {
 
   gulp.watch(path.scssWatch, gulp.series('sass'));
   gulp.watch(path.js).on('change', reload);
-  gulp.watch(path.images).on('change', reload); // ✅ 압축 없음
+  gulp.watch(path.images).on('change', reload);
   gulp.watch(path.html).on('change', reload);
 });
 
@@ -74,22 +89,28 @@ const html = () =>
 const imageBuild = () =>
   gulp
     .src(path.images)
-    .pipe(imagemin()) // ✅ 빌드에서만
-    .pipe(gulp.dest(`${dist}/resource/img`));
+    .pipe(imagemin())
+    .pipe(gulp.dest(`${dist}/resources/img`));
 
 const fonts = () =>
-  gulp.src(path.font).pipe(gulp.dest(`${dist}/resource/fonts`));
+  gulp.src(path.font).pipe(gulp.dest(`${dist}/resources/fonts`));
 
 const sassBuild = () =>
   gulp
-    .src(path.scssEntry, { base: `${src}/resource/scss` })
+    .src(path.scssEntry, { base: `${src}/resources/scss` })
     .pipe(sourcemap.init())
-    .pipe(gulpSass({ outputStyle: 'compressed' }))
-    .pipe(autoprefix({ cascade: false }))
-    .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest(`${dist}/resource/css`));
+    .pipe(gulpSass({ outputStyle: 'expanded' }))
+    .pipe(
+      autoprefix({
+        overrideBrowserslist: ['> 0.2% in KR', 'cover 99.5% in KR', 'not dead'],
+        cascade: false,
+      })
+    )
+    .pipe(postcss([removeEmptyRulesWithComments()]))
+    // .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest(`${dist}/resources/css`));
 
-const js = () => gulp.src(path.js).pipe(gulp.dest(`${dist}/resource/js`));
+const js = () => gulp.src(path.js).pipe(gulp.dest(`${dist}/resources/js`));
 
 /* =========================
    COMMANDS
